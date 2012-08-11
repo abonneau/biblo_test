15d9718aef8aa93f49fe9c2da38054c4328ab63f
Aurelien Bonneau <aurelienbonneau@free.fr>
2012-08-11 12:35:24 +0200

    depot initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BiblioTest::Application.config.secret_token = '9258382bca5f21005d5e094fd1aefbe2b34778639d77d36ad4bb89527120b607afcea95f2327d72f8a673ebdec6c17e881466179f3f3ef9a311052a9bab85dd4'
